a10541c184a989a0298e686c594f7507bed16b2a
Damien Doury <damien.doury@hotmail.fr>
2016-02-28 19:52:09 +0100

    Eevee movement + waiting time

--- Assets/Scripts/PokemonWalk.cs
+++ Assets/Scripts/PokemonWalk.cs
@@ -7,18 +7,32 @@ public class PokemonWalk : MonoBehaviour
     private Vector3 _dest;
     private Transform _tr;
     private NavMeshAgent _nav;
+    private Animator _anim;
+
+    private float _waitTime = 0f;
 
 	void Start()
     {
+        _anim = GetComponent<Animator>();
         _nav = GetComponent<NavMeshAgent>();
         _tr = this.transform;
 	}
 	
 	void Update()
     {
-	    if (_nav.remainingDistance < 0.1f)
+        _anim.SetFloat("Speed", _nav.velocity.magnitude);
+        _waitTime -= Time.deltaTime;
+
+        if (_nav.remainingDistance < 0.1f && _waitTime <= 0f)
         {
-            _nav.SetDestination(new Vector3(Random.Range(-_range, _range), 0f, Random.Range(-_range, _range)));
+            if (Random.value < 0.2f)
+            {
+                _waitTime = Random.Range(2f, 4f);
+            }
+            else
+            {
+                _nav.SetDestination(new Vector3(Random.Range(-_range, _range), 0f, Random.Range(-_range, _range)));
+            }
         }
 	}
 }
