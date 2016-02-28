71c6d33d4cf9d02ca818d0d86e28fa8c62d6d6c4
Damien Doury <damien.doury@hotmail.fr>
2016-02-28 19:20:27 +0100

    First test for catching Pokemons

---
+++ Assets/Scripts/PokemonWalk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PokemonWalk : MonoBehaviour
+{
+    private const float _range = 4f;
+    private Vector3 _dest;
+    private Transform _tr;
+    private NavMeshAgent _nav;
+
+	void Start()
+    {
+        _nav = GetComponent<NavMeshAgent>();
+        _tr = this.transform;
+	}
+	
+	void Update()
+    {
+	    if (_nav.remainingDistance < 0.1f)
+        {
+            _nav.SetDestination(new Vector3(Random.Range(-_range, _range), 0f, Random.Range(-_range, _range)));
+        }
+	}
+}
