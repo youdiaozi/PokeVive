c1a6b38ee565737fbfe0642e9ea3f0c0b9abd5fe
Damien Doury <damien.doury@hotmail.fr>
2016-02-28 16:27:11 +0100

    First commit

---
+++ Assets/Scripts/CatchPokeball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class CatchPokeball : MonoBehaviour
+{
+    public GameObject pokeball;
+    public Rigidbody attachPoint;
+
+    private SteamVR_Controller.Device _device;
+
+    SteamVR_TrackedObject trackedObj;
+    FixedJoint joint;
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        _device = SteamVR_Controller.Input((int)trackedObj.index);
+    }
+
+    void Update()
+    {
+        if (_device != null) return;
+    }
+}
